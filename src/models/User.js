5f29f5409aaf9fcc64cfc33ecd390c864033179a
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2023-03-03 15:46:47 -0300

    req 02

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+
+const User =  (sequelize, DataTypes) => {
+  const User = sequelize.define('User',{
+    id: DataTypes.INTEGER,
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  })
+  return User
+}
+
+module.exports = User;
